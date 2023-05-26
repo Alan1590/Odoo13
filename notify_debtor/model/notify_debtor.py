3f9e0dcc0d0dbc79eb200f186d40c6a8f30c249f
Alan1590 <awg.1590@gmail.com>
2023-05-26 16:11:54 -0300

    Update de modelo, actualizacion de notificaciones

--- notify_debtor/model/notify_debtor.py
+++ notify_debtor/model/notify_debtor.py
@@ -11,42 +11,65 @@ class NotifyDebtor(models.Model):
 	_inherit = 'mail.thread'
 	name = fields.Char("Nombre", size=256, required=True)
 	partner_ids = fields.Many2many("res.partner",readonly=True)
-	journal_id = fields.Many2one("account.journal", required=True)
-	note = fields.Text("Notas")
+	notified_ids = fields.Many2many("notify.debtor.notified",readonly=True)
 	init_date = fields.Date("Fecha inicio",required=True)
 	end_date = fields.Date("Fecha final",required=True)
 
+	def unlink(self):
+		self.env["notify.debtor.notified"].search([('notify_id', '=', self.id),]).unlink()
+		return super(NotifyDebtor, self).unlink()
 
 	def get_invoices(self):
 		invoice_ids = self._get_list_invoices(self.init_date, self.end_date)
 		lpartner =[]
+		lid_invoices = []
 		for inv in invoice_ids:
 			if inv.partner_id.id not in lpartner:
 				lpartner.append(inv.partner_id.id)
+				invoices = self._get_list_invoices_partner(inv.partner_id.id)
+				inv.partner_id.n_invoices = len(invoices)
+				for id_invoice in invoice_ids:
+					lid_invoices.append(id_invoice.id)
+				inv.partner_id.write({'invoices_ids': [(6,0,lid_invoices)] })
 		self.partner_ids = [(6,0,lpartner)]
+
 		return lpartner
-		
+
+	def update(self):
+		ids_notified = self._get_list_notified()
+		self.write({"notified_ids":[(6,0,ids_notified)]})
+
+	def _get_list_notified(self):
+		l_notified = self.env["notify.debtor.notified"].search([('notify_id', '=', self.id),])
+		l_ids = []
+		for ids_notified in l_notified:
+			l_ids.append(ids_notified.id)
+
+		return l_ids
+
 	def _get_list_invoices(self,init_date,end_date):
 		l_invoices = self.env["account.move"].search([("&"),('invoice_date_due', '>', self.init_date),('invoice_date_due', '<', self.end_date)
-			,('state','=','posted'),('type','=','out_invoice'),('journal_id','=',self.journal_id.id),('invoice_payment_state', '!=', 'paid')])
+			,('state','=','posted'),('type','=','out_invoice'),('invoice_payment_state', '!=', 'paid')])
 		return l_invoices
 
-class NotifyDebtorInvoices(models.Model):
+	def _get_list_invoices_partner(self, partner_id):
+		l_invoices = self.env["account.move"].search([("&"),('invoice_date_due', '>', self.init_date),('invoice_date_due', '<', self.end_date)
+			,('state','=','posted'),('type','=','out_invoice'),('invoice_payment_state', '!=', 'paid'),
+			('partner_id', '=', partner_id)])
+		return l_invoices	
+
+
+class NotifyDebtorPartner(models.Model):
 	_inherit = "res.partner"
 	num_notification = fields.Integer("Num envios", readonly=True)
-
-	def _get_linvoices(self,debtor, partner_id):
-		l_invoices = self.env["account.move"].search([("&"),('invoice_date_due', '>', debtor.init_date),('invoice_date_due', '<', debtor.end_date)
-			,('state','=','posted'),('type','=','out_invoice'),('invoice_payment_state', '!=', 'paid'),('journal_id','=',debtor.journal_id.id),('partner_id', '=', partner_id)])
-		return l_invoices
-
+	invoices_ids = fields.Many2many("account.move",readonly=True)
+	n_invoices = fields.Integer("Num facturas", readonly=True)
 
 	def send_wp(self):
 		debtor = self.env["notify.debtor"].search([("id","=",self.env.context.get("parent_id"))])
 
-		l_invoices = self._get_linvoices(debtor,self.id)
 		amount_total = 0
-		for invoices in l_invoices:
+		for invoices in self.invoics_ids:
 			amount_total += invoices.amount_residual
 
 		message = self._get_message(self.name,amount_total,self.email)
@@ -68,9 +91,18 @@ class NotifyDebtorInvoices(models.Model):
 
 		return client_action
 
+	def is_notified(self):
+		return {'type': 'ir.actions.act_window',
+			'name': _('Is notified'),
+			'res_model': 'notify.debtor.notified',
+			'target': 'new',
+			'view_mode': 'form',
+			'context': {'default_notify_id': self.env.context.get("parent_id"), 'default_partner_id': self.id}
+			}
+
 	def _get_message(self,partner_name, amount_total, partner_email):
 		message = """
-		Estimado-a:%20Nos%20comunicamos%20de%20Marinozzi%20Sistemas%20de%20Seguridad,%20tenemos%20registradas%20facturas%20pendientes%20de%20la%20cuenta%20de%20{},%20
+		Estimado-a:%20Nos%20comsunicamos%20de%20Marinozzi%20Sistemas%20de%20Seguridad,%20tenemos%20registradas%20facturas%20pendientes%20de%20la%20cuenta%20de%20{},%20
 		en%20total%20suman%20un%20saldo%20de%20{},%20recuerde%20que%20para%20evitar%20la%20acumulacion%20de%20saldo%20debe%20revisar%20su%20mail%20(allí%20es%20donde%20se%20envían%20todos%20los%20meses%20las%20facturas),%20
 		favor%20de%20confirmar%20si%20es%20correcto%20el%20mail%20que%20tenemos%20registrado%20{}%20le%20informamos%20que%20contamos%20con%20la%20opción%20de%20pago%20
 		por%20Débito%20Automático%20por%20Tarjeta%20de%20Crédito,%20que%20realizamos%20los%20primeros%20días%20del%20mes,%20lo%20invitamos%20a%20que%20se%20sume%20y%20lograr%20mayor%20practicidad.
@@ -92,13 +124,21 @@ class NotifyDebtorInvoices(models.Model):
 			raise ValidationError("La cantidad de digitos no es correcto.")
 		else:
 			prefix = self._get_prefix(self.city)
-			mobile = prefix + mobile[-7:]
+			mobile = prefix +  mobile.strip()[-7:]
 		return mobile.replace("-","")
 
 
 	def _get_prefix(self,city):
 		result = self.env["city.prefix"].search([('name', '=', city),])
 		if result:
-			return result.prefix
+			return "%s%s" %((self.country_id.phone_code), result.prefix)
 		else:
 			raise ValidationError("No existe prefijo para esta ciudad.")
+
+
+class NotifyDebtorNotified(models.Model):
+	_name = "notify.debtor.notified"
+	name = fields.Char("Nombre", size=256, required=True)
+	notify_id = fields.Many2one("notify.debtor", readonly=True)
+	partner_id = fields.Many2one("res.partner",readonly=True)
+
